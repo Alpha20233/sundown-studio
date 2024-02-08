0f83073d2e9ba3bd022c84f354b48b5b7c3515ee
Alpha20233 <deepeshtest2023@gmail.com>
2024-02-08 23:32:49 +0530

    Gsap for angular
    SSR will not cause any issue if you use it in normal or with ssr.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import LocomotiveScroll from 'locomotive-scroll';
 import { ResizeObserver } from '@juggle/resize-observer';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -17,9 +18,16 @@ export class AppComponent {
   scroll;
   @ViewChild('scrollContent') scrollContent: ElementRef;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: object,private el: ElementRef) {}
+
 
   ngAfterViewInit(): void {
+    const myElement = this.el.nativeElement.querySelectorAll('.one');
+    const two = this.el.nativeElement.querySelector('#two');
+
+    gsap.from(myElement, { opacity: 0, y: -500, duration: 1, ease: 'power2.out',delay:0.5});
+    // gsap.from(two,{y:-600,ease:'sine',delay:1.5})
+
     if (isPlatformBrowser(this.platformId)) {
       import('locomotive-scroll').then(lib => {
         this.scroll = new lib.default({
